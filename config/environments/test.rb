84369e1c5bc5472ecef17b96092893085eeb8f1d
adil-paramisoft <adil@paramisoft.com>
2014-03-25 17:56:52 +0530

    intitial commit

---
+++ config/environments/test.rb
@@ -0,0 +1,36 @@
+ViewsDemo::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # The learning environment is used exclusively to run your application's
+  # learning suite. You never need to work with it otherwise. Remember that
+  # your learning database is "scratch space" for the learning suite and is wiped
+  # and recreated between learning runs. Don't rely on the data there!
+  config.cache_classes = true
+
+  # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single learning. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+
+  # Configure static asset server for tests with Cache-Control for performance.
+  config.serve_static_assets  = true
+  config.static_cache_control = "public, max-age=3600"
+
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+
+  # Raise exceptions instead of rendering exception templates.
+  config.action_dispatch.show_exceptions = false
+
+  # Disable request forgery protection in learning environment.
+  config.action_controller.allow_forgery_protection = false
+
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :learning delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :learning
+
+  # Print deprecation notices to the stderr.
+  config.active_support.deprecation = :stderr
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ViewsDemo::Application.config.secret_key_base = 'b7a8c54dc398c91a6b322e899d54a5f1c6e808435827d19eb5548373bd9327c86f915f9638f7b8f3a77936f16bfbde837f2177a37a40253cf2807a2ccd114800'
